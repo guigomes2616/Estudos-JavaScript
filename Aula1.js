0f3ec218337f7b1a65c40e059b1ef608070c449b
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2024-01-30 09:29:42 -0300

    limpeza

--- Aula1.js
+++
@@ -1,4 +0,0 @@
-alert('Minha primeira mensagem'); //Cria uma caixa ao atualizar
-        confirm('Gostou da página?'); // Cria uma caixa com duas opções "OK" e "CANCEL"
-        var nome = window.prompt('Qual seu nome?'); // Cria uma variavel NOME que vai receber o comando WINDOW.PROMPT
-        window.prompt('É um prazer te conhecer, ' + nome); // + vai servir para juntar as frases
--- Aula3.js
+++
@@ -1,7 +0,0 @@
-var nome = window.prompt('Qual o seu nome?')
-document.write(`Olá, ${nome}!Seu nome tem ${nome.length} letras.<br>`)
-// "document.write()" Faz aparecer a mensagem na página
-// "$nome.length" Conta o numero de letras
-
-document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}.<br>`)
-document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}`)
